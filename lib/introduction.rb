4e51c68774f91172086248441c895ed80563d544
jocular-inspector-4517 <shivbhatt@hotmail.com>
2019-02-25 19:55:38 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,4 +6,4 @@ def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-#In this first lab, I have defined two methods: #introduction and 
+#In this first lab, I have defined two methods: #introduction and #introduction_with_language. Both of these methods required arguments and string interpolation which is used in the logic so when the methods are run, #introduction will output a greeting message the users name and #introduction_with_language will output a message which incoroprates the users name and the language they are learning to program. In both instances, the argument requirements have been met. 
